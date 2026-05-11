05dfa4dd0c1a02a13a2838fcdc08a0c7bcd02ceb
suyasuya0futon <225459673+suyasuya0futon@users.noreply.github.com>
2026-05-11 20:37:24 +0900

    文言を少し変更

--- main.js
+++ main.js
@@ -1558,7 +1558,7 @@ const forestPalette = [0x173326, 0x1f4434, 0x2a563f, 0x12281d, 0x365e3c];
         ship.position.y = tuning.ATMOSPHERE_SPARK_START_Y - 8;
         return;
       }
-      endGame("", "大気圏で爆発しました。");
+      endGame("？", "何かに衝突しました。");
     }
 
     function updateAtmosphereDanger(dt) {
